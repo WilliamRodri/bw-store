e56b185263214fdcfca31c1c88622099605ec2e7
WilliamRodri <williamrodrigues.sousa@hotmail.com>
2024-08-30 12:25:29 -0300

    feat correcao currentDate

--- src/views/tables/TableNovaVenda.tsx
+++ src/views/tables/TableNovaVenda.tsx
@@ -15,11 +15,8 @@ const TableNovaVenda = () => {
     const router = useRouter();
 
     const getCurrentDate = () => {
-        const today = new Date();
-        const year = today.getFullYear();
-        const month = String(today.getMonth() + 1).padStart(2, '0');
-        const day = String(today.getDate()).padStart(2, '0');
-        return `${year}-${month}-${day}`;
+        const now = new Date();
+        return now.toISOString().split('T')[0];
     };
 
     const [newSale, setNewSale] = useState<any>({
